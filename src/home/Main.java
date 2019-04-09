6343cdc886aa1b7c4dbf5d058c4e77e5b3c71748
Edwin Rebolledo <wedwin53@gmail.com>
2019-04-09 11:07:00 -0600

    Ajuste de Proyecto para netbeans

--- src/home/Main.java
+++ src/home/Main.java
@@ -1,13 +1,14 @@
 package home;
 
+import forms.Registro;
 import vendedor.Vendedor;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
+		Registro registro = new Registro();
+                registro.setVisible(true);
 	}
 
 }
